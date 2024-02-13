b329edee0804ddb7d573ee8419b9377a54d195de
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-02-13 04:13:38 -0400

    adding loading.tsx global file

---
+++ client/src/app/loading.tsx
@@ -0,0 +1,15 @@
+import { SpinnerCircular } from "spinners-react";
+
+export default function SpinnerLoading() {
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <div className="spinner">
+            </div>
+        </div>
+
+    );
+}
+
+// export default function Loading() {
+//     return <div>Loading...</div>
+// }
