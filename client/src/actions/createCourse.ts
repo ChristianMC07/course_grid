277d163e606d3e3f9f219b93ea61809f20d804d5
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-02-20 07:26:01 -0400

    createCourse server action

--- client/src/actions/createCourse.ts
+++ client/src/actions/createCourse.ts
@@ -1,63 +1,149 @@
 'use server'
 
-import { MongoClient } from "mongodb"
+import { InsertOneResult, MongoClient, UpdateResult } from "mongodb"
 import { redirect } from "next/navigation";
-
-import { writeFile } from "fs/promises";
-import { join } from "path";
 import sanitize from "sanitize-html";
-import { fileURLToPath } from "url";
+import { auth } from '@clerk/nextjs';
+import { createImage } from '@/tools/Toolkit';
+import { Accounts, User } from "@/tools/data.model";
 
 const MONGO_URL: string = "mongodb://mongo:27017/";
 const MONGO_DB_NAME: string = "dbGrids";
 const MONGO_COLLECTION_ACCOUNT: string = "accounts";
 
-type ErrorMessage = { [key: string]: string };
+type ErrorMessage = {
+    [key: string]: any;
+};
+
+
 
 let mongoClient: MongoClient = new MongoClient(MONGO_URL);
 
-export async function createCourse(formState: { errorMessages: ErrorMessage[] }, formData: FormData) {
+export async function createCourse(formState: ErrorMessage, formData: FormData) {
 
-    let errorMessages: ErrorMessage[] = [];
 
-    let _id: string = formData.get('userID') as string;
-    let courseID: any = formData.get('courseCode');
+    let { userId } = auth();
+    console.log(userId);
+
+    let errorMessages: ErrorMessage = {
+        courseIDError: '',
+        courseNameError: '',
+        courseDescriptionError: '',
+        coursePhotoError: '',
+    }
+
+    let _id: string = userId!;
+    let courseID: any = formData.get('courseID');
     let courseName: any = formData.get('courseName');
     let courseDescription: any = formData.get('courseDescription');
     let coursePhoto: File | null = formData.get('coursePhoto') as unknown as File;
 
     let coursePhotoName: string | null = coursePhoto.name;
 
-    if (!coursePhoto) {
-        errorMessages.push({ 'imageError': 'No image uploaded' });
-    } else {
-        const bytes = await coursePhoto.arrayBuffer();
-        const buffer = Buffer.from(bytes);
-
-        const path = join('/', 'tmp', coursePhotoName);
-
-        await writeFile(path, buffer);
-
-        console.log(`open ${path} to see the uploaded file`);
-
-        coursePhotoName = sanitize(coursePhotoName);
-    }
-
     if (typeof courseID !== 'string' || courseID.length == 0) {
-        errorMessages.push({ 'courseIDError': "Please provide a valid Course Code" })
+        errorMessages.courseIDError = 'Please provide a valid Course Code';
     } else {
         courseID = sanitize(courseID);
     }
 
     if (typeof courseName !== 'string' || courseName.length == 0) {
-        errorMessages.push({ 'courseNameError': "Please provide a valid Course Name" })
+        errorMessages.courseNameError = 'Please provide a valid Course Name';
     } else {
         courseName = sanitize(courseName);
     }
 
     if (typeof courseDescription !== 'string' || courseDescription.length == 0) {
-        errorMessages.push({ 'courseDescriptionError': "Please provide a valid Course Description" })
+        errorMessages.courseDescriptionError = 'Please provide a valid Course Description';
     } else {
         courseDescription = sanitize(courseDescription);
     }
+
+    if (!coursePhoto || coursePhotoName == undefined) {
+        errorMessages.coursePhotoError = 'No image uploaded';
+
+    } else {
+        coursePhotoName = sanitize(coursePhoto.name);
+    }
+
+    if (Object.values(errorMessages).join('') === '') {
+        createImage(coursePhoto);
+
+
+        try {
+
+            await mongoClient.connect();
+
+            const accountsCollection = mongoClient.db(MONGO_DB_NAME).collection<User>(MONGO_COLLECTION_ACCOUNT);
+
+            const doc = await accountsCollection.findOne({ _id: _id })
+
+            const courseExist = await accountsCollection.findOne({ courses: { $elemMatch: { 'courseID': courseID } } });
+
+            if (doc == null) {
+                let result: InsertOneResult = await accountsCollection.insertOne({
+                    _id: _id,
+                    courses: [
+                        {
+                            courseID: courseID,
+                            courseName: courseName,
+                            courseDescription: courseDescription,
+                            coursePhoto: coursePhotoName,
+                        }
+
+                    ]
+
+                });
+
+            } else if (doc !== null && courseExist === null) {
+                //The document exist (user), but the course doesn't. Create a new course
+
+                const updateResult = await accountsCollection.updateOne(
+                    { _id: _id },
+                    {
+                        $push: {
+                            courses: {
+                                courseID: courseID,
+                                courseName: courseName,
+                                courseDescription: courseDescription,
+                                coursePhoto: coursePhotoName,
+                            }
+                        }
+                    }
+                );
+
+            } else if (doc !== null && courseExist != null) {
+                // The document and the course both exist - update the existing course
+                const updateResult = await accountsCollection.updateOne(
+                    { _id: _id, "courses.courseID": courseID },
+                    {
+                        $set: {
+                            "courses.$.courseName": courseName,
+                            "courses.$.courseDescription": courseDescription,
+                            "courses.$.coursePhoto": coursePhotoName,
+                        }
+                    }
+                );
+
+                updateResult.modifiedCount === 1 ? console.log("The course was added") : console.log('No luck');
+            }
+
+
+
+        } catch (error) {
+            console.log('This is the error and will appear in the server : ' + error)
+
+        } finally {
+
+            mongoClient.close();
+        }
+
+        redirect('/home/courses/');
+
+
+    } else {
+        return errorMessages;
+    }
+
+
+
 }
