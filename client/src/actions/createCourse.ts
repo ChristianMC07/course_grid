bb7bfc5ecf4f98c4461f95bb3a398158400a7a9e
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-02-20 00:08:40 -0400

    creating server action createCourse.ts

---
+++ client/src/actions/createCourse.ts
@@ -0,0 +1,9 @@
+'use server'
+
+import { MongoClient } from "mongodb"
+
+const MONGO_URL: string = "mongodb://mongo:27017/";
+const MONGO_DB_NAME: string = "dbGrids";
+const MONGO_COLLECTION_ACCOUNT: string = "accounts";
+
+let mongoClient: MongoClient = new MongoClient(MONGO_URL);
