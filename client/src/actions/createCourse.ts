aa02651c6866e6fe898f3a9114b5c7d58c49475c
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-02-20 00:48:07 -0400

    creating array of errors

--- client/src/actions/createCourse.ts
+++ client/src/actions/createCourse.ts
@@ -3,18 +3,27 @@
 import { MongoClient } from "mongodb"
 import { redirect } from "next/navigation";
 
+import { writeFile } from "fs/promises";
+import { join } from "path";
+
 const MONGO_URL: string = "mongodb://mongo:27017/";
 const MONGO_DB_NAME: string = "dbGrids";
 const MONGO_COLLECTION_ACCOUNT: string = "accounts";
 
 let mongoClient: MongoClient = new MongoClient(MONGO_URL);
 
-export async function createCourse(formState: { message: string | null }, formData: FormData) {
+export async function createCourse(formState: { errorMessages: string[] }, formData: FormData) {
+
+    const errorMessages: string[] = [];
 
     const _id: any = formData.get('userID');
     const courseID: any = formData.get('courseCode');
     const courseName: any = formData.get('courseName');
     const courseDescription: any = formData.get('courseDescription');
-    const coursePhoto: any = formData.get('courseDescription');
+    const coursePhoto: File | null = formData.get('coursePhoto') as unknown as File;
+
+    if (!coursePhoto) {
+        errorMessages.push('No image uploaded');
+    }
 
 }
