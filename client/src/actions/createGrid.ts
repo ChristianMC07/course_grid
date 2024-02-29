2aa71998d5268f9e6e5b5bb42cfb2a65363e075e
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-02-29 17:31:28 -0400

    base of createGrid server action

---
+++ client/src/actions/createGrid.ts
@@ -0,0 +1,12 @@
+'use server'
+
+import { User } from "@/tools/data.model";
+import { MongoClient } from "mongodb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import sanitize from "sanitize-html";
+
+const MONGO_URL: string = "mongodb://mongo:27017/";
+const MONGO_DB_NAME: string = "dbGrids";
+const MONGO_COLLECTION_ACCOUNT: string = "accounts";
+
