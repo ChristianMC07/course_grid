0dffbe9e9d900ae837044408aa5d311257a3264e
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-02-29 17:59:07 -0400

    creating the create grid routing

--- client/src/app/home/courses/[id]/grids/page.tsx
+++ client/src/app/home/courses/[id]/grids/page.tsx
@@ -15,7 +15,7 @@ export default async function GridsPage(props: GridsPageProps) {
             <div className="mb-10">
                 <div className="flex items-center mb-6 gap-x-6">
                     <h1 className="text-3xl font-bold text-gray-800">{props.params.id} - Grids</h1>
-                    <Link href="/home/courses/create" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <Link href={`/home/courses/${props.params.id}/grids/create`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         + Add Grid
                     </Link>
                 </div>
