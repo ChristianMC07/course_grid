6803815e6fb6a00214d7d21c12230a38a8ba9d70
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-03-04 16:20:45 -0400

    checking if component is called in the viewgrids

--- client/src/app/home/courses/[id]/grids/[index]/view/page.tsx
+++ client/src/app/home/courses/[id]/grids/[index]/view/page.tsx
@@ -1,15 +1,21 @@
+import Week from "@/app/components/week";
+
 interface ViewGridsProps {
     params: {
         id: string,
         index: string,
     }
-}
+};
+
 
 export default function ViewGrids(props: ViewGridsProps) {
 
     return (
         <div className="min-h-[85vh] bg-gray-100 px-10 pt-10">
             <h1 className="text-3xl font-bold text-gray-800">{props.params.id} - {props.params.index} </h1>
+
+            <Week />
+
         </div>
     )
 }
