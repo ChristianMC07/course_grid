287e430dfef52a263b8826c8ea7d9164720447fa
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-03-04 16:06:19 -0400

    showing some content in the grid page

--- client/src/app/home/courses/[id]/grids/[index]/view/page.tsx
+++ client/src/app/home/courses/[id]/grids/[index]/view/page.tsx
@@ -1,5 +1,15 @@
-export default function ViewGrids() {
+interface ViewGridsProps {
+    params: {
+        id: string,
+        index: string,
+    }
+}
+
+export default function ViewGrids(props: ViewGridsProps) {
+
     return (
-        <div>Grids design</div>
+        <div className="min-h-[85vh] bg-gray-100 px-10 pt-10">
+            <h1 className="text-3xl font-bold text-gray-800">{props.params.id} - {props.params.index} </h1>
+        </div>
     )
 }
