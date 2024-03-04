3da77ebb3c698cba768ed011872f6231989611bd
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-03-03 21:27:56 -0400

    returning grids

--- client/src/actions/thisUserGrids.ts
+++ client/src/actions/thisUserGrids.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { User } from '@/tools/data.model';
+import { User, Grid } from '@/tools/data.model';
 import { auth } from '@clerk/nextjs';
 import { MongoClient } from "mongodb"
 
@@ -11,7 +11,7 @@ const MONGO_COLLECTION_ACCOUNT: string = "accounts";
 let mongoClient: MongoClient = new MongoClient(MONGO_URL);
 
 
-export async function thisCourseGrids(courseID: string) {
+export async function thisCourseGrids(courseID: string): Promise<Grid[] | undefined> {
     let { userId } = auth();
 
     try {
@@ -25,9 +25,18 @@ export async function thisCourseGrids(courseID: string) {
             }
         }
 
-        const userGrids = await accountsCollection.findOne({ _id: userId! });
+        const userInfo = await accountsCollection.findOne({ _id: userId! }, { projection: projection });
 
+        console.log(userInfo);
 
+        if (userInfo && userInfo.courses && userInfo.courses.length > 0) {
+            const grids = userInfo.courses[0].grids;
+            return grids;
+
+        } else {
+            console.log("Course or grids not found.");
+            return [];
+        }
 
     } catch {
 
