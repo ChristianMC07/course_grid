6cd487426a5f0602f3b1698e9f754137c71f3aad
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-03-21 21:40:24 -0300

    calling mongoClient

--- client/src/actions/deleteRow.ts
+++ client/src/actions/deleteRow.ts
@@ -4,3 +4,9 @@ import { User, Grid, Week, Row } from "@/tools/data.model";
 import { MongoClient, UpdateResult } from "mongodb";
 import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
+
+const MONGO_URL: string = "mongodb://mongo:27017/";
+const MONGO_DB_NAME: string = "dbGrids";
+const MONGO_COLLECTION_ACCOUNT: string = "accounts";
+
+let mongoClient: MongoClient = new MongoClient(MONGO_URL);
