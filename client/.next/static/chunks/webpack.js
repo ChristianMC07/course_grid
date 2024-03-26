507ada3d536a0e872db0ef28d508b0d417b8cffd
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-03-26 09:55:40 -0300

    ignoring next folder

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "588cb01c668b9571"; }
+/******/ 		__webpack_require__.h = function() { return "a91a70ab6f22b9bf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
