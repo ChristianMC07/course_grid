3c430d0cecb6cfd92b5bed6bed46bd532aa2006e
Christian Marcelo <christian.marcelo.castro@gmail.com>
2024-04-08 16:04:10 -0300

    adding grids to the breadcrumbs

--- client/src/app/home/courses/[id]/grids/[index]/view/page.tsx
+++ client/src/app/home/courses/[id]/grids/[index]/view/page.tsx
@@ -1,6 +1,8 @@
 import { Grid } from "@/tools/data.model";
 import { gridWeeks } from "@/actions/gridWeeks";
 import WeekComp from "@/app/components/week";
+import Link from "next/link";
+import HomeIcon from "@/app/components/HomeIcon";
 
 interface ViewGridsProps {
     params: {
@@ -16,6 +18,18 @@ export default async function ViewGrids(props: ViewGridsProps) {
 
     return (
         <div className="min-h-[85vh] bg-gray-100 px-10 pt-10">
+
+            <div className="flex items-center gap-x-2 pb-4">
+                <Link className='text-blue-700 hover:text-blue-500 pb-1' href="/home">
+                    <HomeIcon className="fill-current h-6 w-6" />
+                </Link>
+                <span>/</span>
+                <Link className='text-blue-700 hover:text-blue-500' href='/home/courses'>Courses</Link>
+                <span>/</span>
+                <Link className='text-blue-700 hover:text-blue-500' href={`/home/courses/${props.params.id}/grids`}>Grids</Link>
+            </div>
+
+
             <h1 className="text-3xl font-bold text-gray-800">{props.params.id} - {getAllWeekInfo.gridName} </h1>
 
             <WeekComp gridInfo={getAllWeekInfo} courseID={props.params.id} gridID={props.params.index} />
