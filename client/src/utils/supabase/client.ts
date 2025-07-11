af74fc31141178fac482c42ca9feb153f9f71812
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-07-11 13:14:32 -0300

    Fix typo in Supabase URL environment variable

--- client/src/utils/supabase/client.ts
+++ client/src/utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js"
 
 export function createSupabaseClient(token: string | null) {
     const supabase = createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_ULR!,
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
             global: {
